6537a5b6b2b9c81113db9dd6a2d9c9dc5d02cd6c
Chris McKee <chris@chrismckee.co.uk>
2023-05-19 18:03:10 +0100

    correct validate and set verbose on verbose param so cat out entire config isnt default

--- deploy.sh
+++ deploy.sh
@@ -56,12 +56,16 @@ die() {
 parse_params() {
   # default values of variables set from params
   validate=false
+  verbose=false
   env_file=''
 
   while :; do
     case "${1-}" in
     -h | --help) usage ;;
-    -v | --verbose) set -x ;;
+    -v | --verbose) 
+      set -x 
+      verbose=true
+      ;;
     --no-color) NO_COLOR=1 ;;
     -e | --env-file)
       env_file="${2-}"
@@ -125,9 +129,11 @@ kube_subst() {
   # check if the yaml is valid
   if [ $? -eq 0 ]; then
     msg "${GREEN} ---  YAML file is valid"
-    cat $output_file &
-    cat_pid=$!
-    wait "$cat_pid"
+    if [ $verbose == true ]; then
+      cat $output_file &
+      cat_pid=$!
+      wait "$cat_pid"
+    fi
   else
     # remove the invalid file
     rm $output_file
@@ -213,7 +219,7 @@ deploy_to_k8s() {
 
   rancher kubectl apply -f "$1"
 
-  if [ $validate == true ]; then
+  if [ $validate == false ]; then
     return 0
   fi
 
